e1498978c33230c357a53a0c5913c0c12d8cee2f
Devin Smith <github@arzynik.com>
2015-12-13 13:03:24 -0800

    composer inclusion for predis

--- libs/redis.php
+++ libs/redis.php
@@ -1,7 +1,9 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
 if (getenv('REDIS_URL')) {
-	$redis = new Predis\Client([
+	$redis = new \Predis\Client([
 		'host' => parse_url($_ENV['REDIS_URL'], PHP_URL_HOST),
 		'port' => parse_url($_ENV['REDIS_URL'], PHP_URL_PORT),
 		'password' => parse_url($_ENV['REDIS_URL'], PHP_URL_PASS),
